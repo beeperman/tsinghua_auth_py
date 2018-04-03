398756e3ea73659c2623fe2f3223b079cdb1f5af
Fangda Gu <beeperman@live.com>
2018-04-03 12:08:44 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# tsinghua_auth_py
+A simple python3 headless browser script that helps you automatically login the new tsinghua auth. (Wired)
+
+Python package needed
+
+- selenium
+
+System package needed
+
+- PhantomJS
---
+++ login.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from selenium import webdriver
+import os
+
+# use your own credentials
+usernm = 'username'
+passwd = 'password'
+url = 'http://auth4.tsinghua.edu.cn'
+
+browser = webdriver.PhantomJS(service_log_path=os.devnull)
+
+browser.get(url)
+browser.implicitly_wait(3)
+
+is_pswd_found = False
+try:
+    # do login
+    username = browser.find_element_by_id('username')
+    password = browser.find_element_by_id('password')
+    connect = browser.find_element_by_id('connect')
+    is_pswd_found = True
+
+    username.clear()
+    username.send_keys(usernm)
+
+    password.clear()
+    password.send_keys(passwd)
+
+    connect.submit()
+    print('successfully pressed connect button')
+
+    # check login status
+    browser.get(url)
+    browser.implicitly_wait(3)
+
+    disconnect = browser.find_element_by_class_name('disconnect')
+    print('login success')
+except Exception as e:
+    # some failure
+    if is_pswd_found:
+        print(e.__str__())
+    else:
+        print('already logged in.')
+
+browser.close()
