58c45bfad6f77c60bb2dcfa14f76a2554eddb391
Fangda Gu <beeperman@live.com>
2018-04-07 20:17:23 +0800

    added re-login

--- login.py
+++ login.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 from selenium import webdriver
+import time
 import sys
 import os
 
 # off-campus network access
 off_camp = True
+
+# re-login time (in hour)
+re_login_time = 0
 
 # use your own credentials
 usernm = 'username'
@@ -25,45 +29,69 @@ xor = lambda x, y: bool(x) != bool(y)
 browser = webdriver.PhantomJS(service_log_path=os.devnull)
 #browser = webdriver.Firefox()
 
-browser.get(url)
-browser.implicitly_wait(3)
-
-is_pswd_found = False
-try:
-    # network access type
-    off_campus = browser.find_element_by_name('save_me')
-    if xor(off_camp, off_campus.is_selected()):
-        off_campus.click()
-    assert(off_camp == off_campus.is_selected())
-    print('set network access type to ' + 'off_campus' if off_camp else 'on_campus only')
-
-    # do login
-    username = browser.find_element_by_id('username')
-    password = browser.find_element_by_id('password')
-    connect = browser.find_element_by_id('connect')
-    is_pswd_found = True
-
-    username.clear()
-    username.send_keys(usernm)
-
-    password.clear()
-    password.send_keys(passwd)
-
-    connect.submit()
-    print('successfully pressed connect button')
-
-    # check login status
+def headless_login():
     browser.get(url)
     browser.implicitly_wait(3)
 
-    disconnect = browser.find_element_by_class_name('disconnect')
-    print('login success')
-except Exception as e:
-    # some failure
-    if is_pswd_found:
-        print('login failure. please check your credentials')
-        print(e.__str__(), file=sys.stderr)
-    else:
-        print('already logged in.')
+    def check_login(auto_logout = False):
+        # check login status helper function
+        browser.get(url)
+        browser.implicitly_wait(3)
+
+        disconnect = browser.find_element_by_id('form3')
+        clock = browser.find_element_by_id('clock')
+
+
+        if auto_logout and re_login_time != 0:
+            hour = clock.text.split(':')[0]
+            if off_camp and (hour == '' or hour == 'NaN'
+                or int(hour) >= re_login_time):
+                print('Has been logged in for ' + clock.text + '. Performing re-login.')
+                disconnect.submit()
+                return True
+
+        return False
+
+    is_pswd_found = False
+    try:
+        # do login
+        username = browser.find_element_by_id('username')
+        password = browser.find_element_by_id('password')
+        connect = browser.find_element_by_id('connect')
+        is_pswd_found = True
+
+        # network access type
+        off_campus = browser.find_element_by_name('save_me')
+        if xor(off_camp, off_campus.is_selected()):
+            off_campus.click()
+        assert (off_camp == off_campus.is_selected())
+        print('set network access type to ' + 'off_campus' if off_camp else 'on_campus only')
+
+        username.clear()
+        username.send_keys(usernm)
+
+        password.clear()
+        password.send_keys(passwd)
+
+        connect.submit()
+        print('successfully pressed connect button')
+
+        time.sleep(2)
+        check_login()
+        print('login success')
+    except Exception as e:
+        # some failure
+        if is_pswd_found:
+            print('login failure. please check your credentials')
+            print(e.__str__(), file=sys.stderr)
+        else:
+            print('already logged in.')
+            return check_login(auto_logout=True)
+    return False
+
+# re-run login if login time exceed time limit
+if headless_login():
+    time.sleep(2)
+    headless_login()
 
 browser.close()
